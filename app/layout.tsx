36177971d994c31c8bca194c06fb1b85f012abc4
bernatto_uec <braian5353@gmail.com>
2023-07-25 15:45:23 -0400

    like button added

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,9 @@ import type { Metadata } from 'next'
 import { Figtree } from 'next/font/google'
 import SupabaseProvider from '@/providers/SupabaseProvider'
 import UserProvider from '@/providers/UserProvider'
-import Modal from '@/components/Modal'
 import ModalProvider from '@/providers/ModalProvider'
 import ToasterProvider from '@/providers/ToasterProvider'
+import getSongsByUserId from '@/actions/getSongsByUserId'
 
 const font = Figtree({ subsets: ['latin'] })
 
@@ -15,11 +15,14 @@ export const metadata: Metadata = {
   description: 'Listen good music anytime!'
 }
 
-export default function RootLayout({
+export const revalidate = 0
+
+export default async function RootLayout({
   children
 }: {
   children: React.ReactNode
 }) {
+  const userSongs = await getSongsByUserId()
   return (
     <html lang='en'>
       <body className={font.className}>
@@ -27,7 +30,7 @@ export default function RootLayout({
         <SupabaseProvider>
           <UserProvider>
             <ModalProvider />
-            <Sidebar>{children}</Sidebar>
+            <Sidebar songs={userSongs}>{children}</Sidebar>
           </UserProvider>
         </SupabaseProvider>
       </body>
--- components/Library.tsx
+++ components/Library.tsx
@@ -1,9 +1,26 @@
 import { TbPlaylist } from 'react-icons/tb'
 import { AiOutlinePlus } from 'react-icons/ai'
+import useAuthModal from '@/hooks/useAuthModal'
+import { useUser } from '@/hooks/useUser'
+import useUploadModal from '@/hooks/useUploadModal'
+import { Song } from '@/types'
+import MediaItem from './MediaItem'
 
-const Library = () => {
+interface LibraryProps {
+  songs: Song[]
+}
+
+const Library = ({ songs }: LibraryProps) => {
+  const authModal = useAuthModal()
+  const uploadModal = useUploadModal()
+  const { user } = useUser()
+
   const onClick = () => {
-    // handle upload later
+    if (!user) {
+      return authModal.onOpen()
+    }
+    // check for subscription
+    return uploadModal.onOpen()
   }
 
   return (
@@ -19,7 +36,11 @@ const Library = () => {
           className='text-neutral-400 cursor-pointer hover:text-white transition'
         />
       </div>
-      <div className='flex flex-col gap-y-2 mt-4 px-3'>List of Songs!</div>
+      <div className='flex flex-col gap-y-2 mt-4 px-3'>
+        {songs.map(item => (
+          <MediaItem onClick={() => {}} key={item.id} data={item} />
+        ))}
+      </div>
     </div>
   )
 }
