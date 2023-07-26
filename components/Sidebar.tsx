50b7502eca531d937a6117adca1d104bf3ee301c
braian-bernatto <braian5353@gmail.com>
2023-07-25 23:13:40 -0400

    play updated

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -8,6 +8,8 @@ import Box from './Box'
 import SidebarItem from './SidebarItem'
 import Library from './Library'
 import { Song } from '@/types'
+import usePlayer from '@/hooks/usePlayer'
+import { twMerge } from 'tailwind-merge'
 
 interface SidebarProps {
   children: React.ReactNode
@@ -15,6 +17,7 @@ interface SidebarProps {
 }
 
 const Sidebar = ({ children, songs }: SidebarProps) => {
+  const player = usePlayer()
   const pathname = usePathname()
   const routes = useMemo(
     () => [
@@ -35,7 +38,9 @@ const Sidebar = ({ children, songs }: SidebarProps) => {
   )
 
   return (
-    <div className='flex h-full'>
+    <div
+      className={twMerge(`flex h-full`, player.activeId && 'h-[calc(100%-80)]')}
+    >
       <div className='hidden md:flex flex-col gap-y-2 bg-black h-full w-[300px] p-2'>
         <Box>
           <div className='flex flex-col gap-y-4 px-5 py-4'>
